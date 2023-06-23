79eff9a6f7bc17604f7322c563e6910aee962d24
Electronica-dev <srsamartha@gmail.com>
2023-06-23 20:04:34 +0530

    add comments

--- index.js
+++ index.js
@@ -13,30 +13,20 @@ app.use(express.json())
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
+//send text request to OpenAI and receives a response back
 app.post('/api/generate', async (req, res) => {
-  const models = await openai.listModels();
-  if (!configuration.apiKey) {
-    res.status(500).json({
-      error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
-    });
-    return;
-  }
-
   try {
     const completion = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [{"role": "user", "content": req.body.text}],
-      // prompt: req.body.text,
       temperature: 0.6,
       max_tokens: 200,
     });
     res.status(200).json({ result: completion.data.choices[0] });
   } catch(error) {
-    // Consider adjusting the error handling logic for your use case
     if (error.response) {
       console.error(error.response.status, error.response.data);
       res.status(error.response.status).json(error.response.data);
